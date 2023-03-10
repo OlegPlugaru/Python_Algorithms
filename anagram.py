c9007a0534245317d53ef0558fad8f148eef8f51
OlegPlugaru <oleg.plugaru.md@gmail.com>
2023-03-10 19:27:07 +0200

    writed an anagram checker

---
+++ anagram.py
@@ -0,0 +1,33 @@
+def anagram(s1, s2):
+    s1 = s1.replace(' ', '').lower()
+    s2 = s2.replace(' ', '').lower()
+
+    # check if same number of letters
+    if len(s1) != len(s2):
+        return False
+
+    # count frequency of each letter
+    count = {}
+
+    for letter in s1:  # for ever letter in first string
+        if letter in count:  # if letter is already in my dictionary, then
+            count[letter] += 1  # add 1 to that letter key
+        else:
+            count[letter] = 1
+
+        # do reverse for second string
+    for letter in s2:
+        if letter in count:
+            count[letter] -= 1
+        else:
+            count[letter] = 1
+
+    for k in count:
+        if count[k] != 0:
+            return False
+
+    return True
+
+
+x = anagram('Clint Eastwood', 'old WEST action')
+print(x)
