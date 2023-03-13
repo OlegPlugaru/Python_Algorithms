8a0aa7ad784fd1d9fdc75bf5d2b202849a604ee6
OlegPlugaru <oleg.plugaru.md@gmail.com>
2023-03-13 20:44:54 +0200

    writed a function who shows minesweeper field and bombms

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-print("hello world")
-print("ok")
-print("you")
-print("dasfaf")
+lst1 = [1, 3, 4, 6, 7, 9]
+lst2 = [1,2,4,5,9,10]
+r = []
+for i in lst1:
+    for j in lst2:
+        if i == j:
+            r.append(i)
+print(r)
